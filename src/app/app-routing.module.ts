7ce4358a2586087b349e81a957b93aea153b4c9a
Luis Ernesto <netohaarp@outlook.com>
2021-03-22 12:46:33 -0600

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { TecnologicosComponent } from './tecnologicos/tecnologicos.component';
 
-//routes
+//routes 2
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'reader', loadChildren: () => import('./reader/reader.module').then(m => m.ReaderModule), canActivate:[AuthGuard]  }, 
